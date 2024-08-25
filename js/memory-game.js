09d4363f19cb7c3168ce0c7acbf72397f56f143f
shrfrn <shrfrn@gmail.com>
2024-08-25 11:54:39 +0300

    Ad basic game logic

--- js/memory-game.js
+++ js/memory-game.js
@@ -4,6 +4,23 @@ function init() {
     console.log('Hi')
 }
 
-function onCardClick(elCard){
-    elCard.classList.add('selected')
+function onCardClick(elCard) {
+	var elSelectedCards = document.querySelectorAll('.selected')
+	if (elSelectedCards.length === 2) return
+
+	elCard.classList.add('selected')
+
+	if (elSelectedCards.length === 0) return
+	else elSelectedCards = document.querySelectorAll('.selected')
+
+	if (elSelectedCards[0].innerText === elSelectedCards[1].innerText) {
+		elSelectedCards.forEach(elCard => {
+			elCard.classList.add('paired')
+			elCard.classList.remove('selected')
+		})
+	} else {
+		setTimeout(() => {
+			elSelectedCards.forEach(elCard => elCard.classList.remove('selected'))
+		}, 2000)
+	}
 }
