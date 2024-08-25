7fae93c98cad8a77916eb81926a02906f9e403eb
reut <reutedry1@gmail.com>
2024-08-25 11:56:41 +0300

    Add dynamic DOM

--- js/memory-game.js
+++ js/memory-game.js
@@ -1,7 +1,40 @@
 'use strict'
 
+
 function init() {
-    console.log('Hi')
+
+	const nums = initNums()
+	const strHtml =
+		nums.map(num => `
+            <article 
+                class="card" 
+                onclick="onCardClick(this)"> <div>${num}</div>
+            </article>`).join('')
+
+	document.querySelector('main').innerHTML = strHtml
+}
+
+function initNums() {
+	const nums = []
+
+	for (var i = 0; i < 20; i++) {
+		nums.push(i % 10)
+	}
+
+	for (var i = 0; i < nums.length; i++) {
+		const idx = getRandomInt(0, nums.length)
+
+		const temp = nums[idx]
+		nums[idx] = nums[i]
+		nums[i] = temp
+	}
+	return nums
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
 }
 
 function onCardClick(elCard) {
